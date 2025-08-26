9cb86b6c3c2c7671b24bf645b0e30abaf46fd2ed
tgalirop <56532101+tgalirop@users.noreply.github.com>
2025-08-26 13:57:45 +0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Text, DateTime, func
-from .database import Base
-
-class Click(Base):
-    __tablename__ = "clicks"
-
-    id = Column(Integer, primary_key=True, index=True)
-    button = Column(Text, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
